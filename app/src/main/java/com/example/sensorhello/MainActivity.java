9d8cf3c9bc77d9be28ce2ee4ba080cb536b933e0
ravedave <dic15dju@student.lu.se>
2019-03-28 11:20:14 +0100

    GUI shell finished

--- app/src/main/java/com/example/sensorhello/MainActivity.java
+++ app/src/main/java/com/example/sensorhello/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sensorhello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button accButton = (Button) findViewById(R.id.button2);
+
+        accButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, AccelerometerActivity.class));
+            }
+        });
+
+        Button compButton = (Button) findViewById(R.id.button);
+
+        compButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, CompassActivity.class));
+            }
+        });
     }
 }
