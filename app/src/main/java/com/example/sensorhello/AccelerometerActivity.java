3adb133f0c8ea0b3bac7d5b2bd6ed9f66815b4d1
ravedave <dic15dju@student.lu.se>
2019-03-28 20:18:57 +0100

    Code cleanup

--- app/src/main/java/com/example/sensorhello/AccelerometerActivity.java
+++ app/src/main/java/com/example/sensorhello/AccelerometerActivity.java
@@ -37,16 +37,6 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
 
     }
 
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
-    }
-
-    protected void onPause() {
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
-
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
